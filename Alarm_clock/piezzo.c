1aad787085cb859e328695b10d23a07b4ba870bf
Michael Doran <doranm09@gmail.com>
2020-04-16 17:16:01 -0400

    adding piezzo init for pwm

--- Alarm_clock/piezzo.c
+++ Alarm_clock/piezzo.c
@@ -1,2 +1,31 @@
 #include "piezzo.h"
 #include "msp.h"
+#include "notes.h"
+
+/*
+ *  Description:
+ *
+ *  This function will initialize timer 16 as a pwm for our piezzo buzzer
+ *
+ */
+void piezzo_init(void)
+{
+    /* Configure P2.4 as Timer A0.1 output */
+    P2->SEL0 |= 0x80;
+    P2->SEL1 &= ~0x80;
+    P2->DIR |= 0x80;
+
+    /* configure TimerA0.4 as PWM */
+    TIMER_A0->CCR[0] = 3826-1;     /* PWM Period */
+    TIMER_A0->CCR[4] = 3826/2;     /* CCR4 PWM duty cycle */
+    TIMER_A0->CCTL[4] = 0xE0;       /* CCR4 reset/set mode */
+    TIMER_A0->CTL = 0x0214;         /* use SMCLK, count up, clear TA0R register */
+
+    NVIC_SetPriority(TA0_N_IRQn, 3);
+    NVIC_EnableIRQ(TA0_N_IRQn);
+}
+
+void TA0_N_IRQHandler(void)
+{
+    TIMER_A0->CCTL[1] &= ~1;
+}
--- Alarm_clock/piezzo.h
+++ Alarm_clock/piezzo.h
@@ -8,8 +8,10 @@
 #ifndef PIEZZO_H_
 #define PIEZZO_H_
 
+#define F_CPU 3000000 // 3MHz
+#define BUZZER 0x10 // BIT 4
 
-
-
+void piezzo_init(void);
+void play_note(int note, int duration);
 
 #endif /* PIEZZO_H_ */
